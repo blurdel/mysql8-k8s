2a7773995399c2d6f808c505b958884f6ee908e6
blurdel <5648737+blurdel@users.noreply.github.com>
2025-03-15 10:36:01 -0700

    fixed PV

--- start-all.sh
+++ start-all.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-kubectl apply -f mysql-config.yaml
 kubectl apply -f mysql-secret.yaml
-# kubectl apply -f pod.yaml
+kubectl apply -f mysql-pv.yaml
 kubectl apply -f mysql.yaml
 kubectl apply -f client-pod.yaml
--- stop-all.sh
+++ stop-all.sh
@@ -2,6 +2,5 @@
 
 kubectl delete -f client-pod.yaml
 kubectl delete -f mysql.yaml
-# kubectl delete -f pod.yaml
+kubectl delete -f mysql-pv.yaml
 kubectl delete -f mysql-secret.yaml
-kubectl delete -f mysql-config.yaml
