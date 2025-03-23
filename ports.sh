eb55df8c4d1788c8ac8351c67bf74a3bcfa570b6
blurdel <5648737+blurdel@users.noreply.github.com>
2025-03-23 10:51:37 -0700

    added mysql-config.yaml

--- ports.sh
+++ ports.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# target=${1:-"service/mysql-service"}
-target="$1"
+target=${1:-"service/mysql-service"}
+# target="$1"
 
 if [[ -z "${target}" ]]; then
     "Usage: $0 <service/service-name>"
     exit
 fi
 
-kubectl port-forward  "${target}" 3306:3306 &
-
-# ports.sh  service/mysql-service 3306:3306 &
+kubectl port-forward  "${target}" 3306:3306
--- stop-all.sh
+++ stop-all.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 kubectl delete -f client-pod.yaml
+
 kubectl delete -f mysql.yaml
 kubectl delete -f mysql-pv.yaml
 kubectl delete -f mysql-secret.yaml
+kubectl delete -f mysql-config.yaml
