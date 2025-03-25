cd255179a0c52745cd03d983b6f899d6c7dd35fc
blurdel <5648737+blurdel@users.noreply.github.com>
2025-03-24 19:58:47 -0700

    added user creation SQL commands

--- database.sql
+++ database.sql
@@ -1,10 +1,20 @@
+-- user/database setup
+create user if not exists 'tmuser'@'%' identified by 'tmuser';
+grant select, insert, update, delete, show view on `SPRING`.* to 'tmuser'@'%';
+-- grant all on *.* to 'tmuser'@'%';
+flush privileges;
+
+-- can verify privileges
+use mysql;
+select Host,User,Select_priv,Insert_priv,Update_priv,Delete_priv,Create_priv,Drop_priv,File_priv,Grant_priv from user;
+
 -- create table and data
 drop database if exists SPRING;
 create database if not exists SPRING;
 use SPRING;
 
 CREATE TABLE Person (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     age INT NOT NULL
 );
